7dafb8d9f8137322ff64f38e98444ac44d22f6cd
Andrey Rakhmanov <rakhmanov.andrey@gmail.com>
2021-04-17 20:13:34 +0300

    Start index.html add header and images slider

---
+++ static/js/index-script.js
@@ -0,0 +1,10 @@
+$(function(){
+  $(".twentytwenty-container").twentytwenty({
+    // default_offset_pct: 0.5, // How much of the before image is visible when the page loads
+    // orientation: 'horizontal', // Orientation of the before and after images ('horizontal' or 'vertical')
+    // before_label: 'Before', // Set a custom before label
+    // after_label: 'After', // Set a custom after label
+    no_overlay: true, //Do not show the overlay with before and after
+    click_to_move: false // Allow a user to click (or tap) anywhere on the image to move the slider to that location.
+  });
+});
