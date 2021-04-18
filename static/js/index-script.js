26b78c9206db892ee3277995d20d936f53c761d6
Andrey Rakhmanov <rakhmanov.andrey@gmail.com>
2021-04-18 23:36:07 +0300

    Make examples images one height and full wide width

--- static/js/index-script.js
+++ static/js/index-script.js
@@ -4,8 +4,7 @@ $(function(){
     // orientation: 'horizontal', // Orientation of the before and after images ('horizontal' or 'vertical')
     // before_label: 'Before', // Set a custom before label
     // after_label: 'After', // Set a custom after label
-    no_overlay: false, //Do not show the overlay with before and after
-    click_to_move: false, // Allow a user to click (or tap) anywhere on the image to move the slider to that location.
-    disappear: true
+    no_overlay: true, //Do not show the overlay with before and after
+    click_to_move: false // Allow a user to click (or tap) anywhere on the image to move the slider to that location.
   });
 });
